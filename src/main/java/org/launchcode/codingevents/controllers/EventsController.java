8aaa243a38ad31aba9782a5dbb826bc1dd42044c
Mark Scheer <mscheer314@gmail.com>
2021-06-01 21:08:21 -0500

    Add Event model

--- src/main/java/org/launchcode/codingevents/controllers/EventsController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventsController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,40 +9,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventsController {
-    private static HashMap<String, String> events = new HashMap<>();
+
+    private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-//        model.addAttribute("events", events);
-//        return "events/index";
-        events.put("Pouring Coffee into the Matrix - Building Java Applications on Neo4j",
-                "Many of us have built applications for traditional data structures (like relational database tables), but is it different for graph data stores? In this session, we will cover a brief intro to graphs and build a typical Java application for a graph database. We will interact with the graph data from the application, and explore some deployment possibilities. Join us to see how to amplify the power of connected data with applications!");
-        events.put("Azure DevOps for .Net- Fall into the Pit of Success with Jeffrey Palermo",
-                "Azure DevOps Services and all of the automation involved for a complete DevOps environment can be daunting. In this talk, Jeffrey Palermo provides prescriptive guidance for developers to fall into the \"pit of success\" when creating automated DevOps pipelines for complex .NET apps targeting Azure.");
-        events.put("Machine Learning for Beginners: Decision Trees",
-                "Join us for a machine learning class designed for beginners! This class is free and open to the public. We will start with the basic overview of machine learning concepts and then dig into the details. The class will be taught using Python but will focus on machine learning concepts more than coding.\n" +
-                        "\n" +
-                        "In this class we will cover the basics of decision and regression trees and their use in predictive " +
-                        "modeling. If time permits we will begin our discussion of ensemble methods using decision trees by covering the concepts of boosting and bagging.");
-
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String renderCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
-        events.put(eventName, eventDescription );
+        events.add(new Event(eventName, eventDescription));
         return "redirect:";
     }
 }
---
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -0,0 +1,34 @@
+package org.launchcode.codingevents.models;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Event {
+    private String name;
+    private String description;
+
+    public Event(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override public String toString() {
+        return name;
+    }
+}
