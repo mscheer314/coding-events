c8c6f899e025980484700b871fc734767cebfc5e
Mark Scheer <mscheer314@gmail.com>
2021-06-01 21:19:02 -0500

    Add fields and equals()/hashCode() to model

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,15 +1,22 @@
 package org.launchcode.codingevents.models;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Objects;
 
 public class Event {
+    private static int nextId = 1;
+    private final int id;
     private String name;
     private String description;
 
     public Event(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextId;
+        nextId++;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -30,5 +37,16 @@ public class Event {
 
     @Override public String toString() {
         return name;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override public int hashCode() {
+        return Objects.hash(id);
     }
 }
